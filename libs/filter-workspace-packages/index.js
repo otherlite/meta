15ecf58aaf2d73507602c0d2dec10b45860aa939
lite.zeng <lite.zeng@bybit.com>
2022-12-26 17:16:14 +0800

    fix: cicd (#8)

--- libs/filter-workspace-packages/index.js
+++ libs/filter-workspace-packages/index.js
@@ -1,21 +1,37 @@
-const { exec, echo, exit, ShellString } = require("shelljs");
-const { code, stdout, stderr } = exec(
-  "pnpm list --filter=...[refs/remotes/origin/main] --json",
-  {
-    silent: true,
-  }
+const { exec, echo, exit } = require("shelljs");
+
+console.log(
+  "GITHUB_PULL_REQUEST_BASE_SHA",
+  process.env.GITHUB_PULL_REQUEST_BASE_SHA
 );
+console.log("GITHUB_EVENT_BEFORE", process.env.GITHUB_EVENT_BEFORE);
+
+const lastCommitId =
+  process.env.GITHUB_PULL_REQUEST_BASE_SHA ||
+  process.env.GITHUB_EVENT_BEFORE ||
+  "refs/remotes/origin/main";
+
+const execCommand = `pnpm list --filter=...[${lastCommitId}] --json`;
+console.log(execCommand);
+const { code, stdout, stderr } = exec(execCommand);
 
 if (code !== 0) {
   echo("Error: pnpm list failed");
   echo("Program stderr:", stderr);
-  exit(1);
 }
 
-exec(
-  `echo "packages=${JSON.stringify(
+let packages;
+try {
+  packages = JSON.stringify(
     JSON.parse(stdout)
       .map((item) => item.name)
-      .filter((item) => item)
-  ).replace(/\"/g, '\\"')}" >> $GITHUB_OUTPUT`
-);
+      .filter((item) => item) || []
+  ).replace(/\"/g, '\\"');
+} catch (error) {
+  console.log(error);
+  packages = "[]";
+}
+
+const execCommand2 = `echo "packages=${packages}" >> $GITHUB_OUTPUT`;
+console.log(execCommand2);
+exec(execCommand2);
