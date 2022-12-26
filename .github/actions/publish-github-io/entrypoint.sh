f9a12b7f309a72c142a51ec849783172d427630b
lite.zeng <lite.zeng@bybit.com>
2022-12-27 03:16:36 +0800

    xx

--- .github/actions/publish-github-io/entrypoint.sh
+++ .github/actions/publish-github-io/entrypoint.sh
@@ -7,29 +7,22 @@ echo ''
 echo "==> change directory to the dest"
 git config --global --add safe.directory "*"
 cd $PUBLISH_DIR
-echo "==> 1111"
-pwd
-ls -a
-echo "==> 222"
-echo "==> prepare to deploy"
-git init
-echo "==> 333"
-git --version
-# git config --global --add safe.directory "*"
-git status
-# git config user.name "${GITHUB_ACTOR}"
-# git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-# if [ -z "$(git status --porcelain)" ]; then
-#     echo "The BUILD_DIR is setting error or nothing produced" && \
-#     echo "Exiting..."
-#     exit 0
-# fi
 
-# echo "==> starting deploying"
-# git add .
-# git commit -m "Auto deploy from Github Actions"
-# git push --force https://username:${ACCESS_TOKEN}@github.com/${TARGET_REPO}.git master:$TARGET_BRANCH
-# rm -fr .git
+echo "==> prepare deploying"
+git init
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
+if [ -z "$(git status --porcelain)" ]; then
+    echo "The BUILD_DIR is setting error or nothing produced" && \
+    echo "Exiting..."
+    exit 0
+fi
+
+echo "==> starting deploying"
+git add .
+git commit -m "Auto deploy from Github Actions"
+git push --force https://username:${ACCESS_TOKEN}@github.com/${TARGET_REPO}.git master:$TARGET_BRANCH
+rm -fr .git
 cd $GITHUB_WORKSPACE
 echo "Successfully deployed!" && \
 echo "See: https://github.com/$TARGET_REPO/tree/$TARGET_BRANCH"
