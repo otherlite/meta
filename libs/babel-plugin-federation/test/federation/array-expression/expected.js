8c3064af80c9014e3f6304820c6ed8518065596d
lite.zeng <lite.zeng@bybit.com>
2022-12-27 23:50:16 +0800

    feat: babel-plugin-federation迁移

---
+++ libs/babel-plugin-federation/test/federation/array-expression/expected.js
@@ -0,0 +1,9 @@
+"use strict";
+
+var _antd = require("common/antd");
+
+var a = [_antd.Button];
+var b = {
+  'test': [_antd.Button]
+};
+[_antd.Button].map(function () {});
---
+++ libs/babel-plugin-federation/test/mixed/custom-style-path-ignore/expected.js
@@ -0,0 +1,15 @@
+"use strict";
+
+var _animation = _interopRequireDefault(require("common/antd/lib/animation"));
+
+require("common/antd/lib/button/style/2x");
+
+var _button = _interopRequireDefault(require("common/antd/lib/button"));
+
+var _react = _interopRequireDefault(require("react"));
+
+var _reactDom = _interopRequireDefault(require("react-dom"));
+
+function _interopRequireDefault(obj) { return obj && obj.__esModule ? obj : { default: obj }; }
+
+ReactDOM.render( /*#__PURE__*/_react.default.createElement(_animation.default, null, /*#__PURE__*/_react.default.createElement(_button.default, null, "xxxx")), document.getElementById("react-container"));
