e98a65d064d0b97cf5da28b93c79bbad39c86b66
otherlite <597790837@qq.com>
2022-12-31 19:43:11 +0800

    feat: 404

--- .github/actions/publish-github-io/entrypoint.sh
+++ .github/actions/publish-github-io/entrypoint.sh
@@ -4,25 +4,35 @@ set -e
 
 echo ''
 
-echo "==> change directory to the dest"
+TEMP_DIR=".tmp_dir"
 git config --global --add safe.directory "*"
-cd $PUBLISH_DIR
 
 echo "==> prepare deploying"
-git init
-git config user.name "${GITHUB_ACTOR}"
-git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
-if [ -z "$(git status --porcelain)" ]; then
-    echo "The BUILD_DIR is setting error or nothing produced" && \
-    echo "Exiting..."
-    exit 0
-fi
+if [ -z "$TARGET_REPO_DIR" ]; then 
+  echo "TARGET_REPO_DIR: "
+  cp -vfr $BUILD_DIR $TEMP_DIR
+elif [ "$TARGET_REPO_DIR" = "/" ]; then    
+  echo "TARGET_REPO_DIR: /"
+  git clone https://github.com/$TARGET_REPO.git $TEMP_DIR
+  rm -fr $TEMP_DIR/.git
+  cp -vfr $BUILD_DIR/* $TEMP_DIR # $TEMP_DIR已存在时需要使用 $BUILD_DIR/*
+else   
+  echo "TARGET_REPO_DIR: $TARGET_REPO_DIR"
+  git clone https://github.com/$TARGET_REPO.git $TEMP_DIR
+  rm -fr $TEMP_DIR/.git
+  rm -fr $TEMP_DIR/$TARGET_REPO_DIR
+  cp -vfr $BUILD_DIR $TEMP_DIR/$TARGET_REPO_DIR
+fi 
 
 echo "==> starting deploying"
+cd $TEMP_DIR
+git init 
+git config user.name "${GITHUB_ACTOR}"
+git config user.email "${GITHUB_ACTOR}@users.noreply.github.com"
 git add .
 git commit -m "Auto deploy from Github Actions"
-git push --force https://username:${ACCESS_TOKEN}@github.com/${TARGET_REPO}.git master:$TARGET_BRANCH
+git push --force https://username:${ACCESS_TOKEN}@github.com/${TARGET_REPO}.git master:$TARGET_REPO_BRANCH
 rm -fr .git
 cd $GITHUB_WORKSPACE
 echo "Successfully deployed!" && \
-echo "See: https://github.com/$TARGET_REPO/tree/$TARGET_BRANCH"
+echo "See: https://github.com/$TARGET_REPO/tree/$TARGET_REPO_BRANCH"
