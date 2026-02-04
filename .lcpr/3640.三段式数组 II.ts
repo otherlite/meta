be90b44cfff623e59f257a2734c5faead1e41a6c
YuWen Zeng <yuwen.z@decode-group.com>
2026-02-04 11:31:50 +0800

    feat: add problem 3640 trionic array II with initial solution stub
    Add new LeetCode problem "3640.三段式数组 II" (Trionic Array II) with initial TypeScript solution stub. This includes the problem description, examples, and constraints. Also initialize necessary lcpr configuration files and cache for the problem.

---
+++ .lcpr/3640.三段式数组 II.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=3640 lang=typescript
+ * @lcpr version=30307
+ *
+ * [3640] 三段式数组 II
+ */
+
+// @lc code=start
+function maxSumTrionic(nums: number[]): number {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [0,-2,-1,-3,0,2,-1]\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1,4,2,7]\n
+// @lcpr case=end
+
+ */
+
