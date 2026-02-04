1db2f85e72a661f5ecd98c2287eba9e6534fb684
YuWen Zeng <yuwen.z@decode-group.com>
2026-02-04 11:44:22 +0800

    chore: clean up LeetCode problem files and configs
    Remove unused LeetCode plugin configurations and problem files.
    Add new empty template for problem "1. Two Sum".

---
+++ .lcpr/1.两数之和.ts
@@ -0,0 +1,30 @@
+/*
+ * @lc app=leetcode.cn id=1 lang=typescript
+ * @lcpr version=30307
+ *
+ * [1] 两数之和
+ */
+
+// @lc code=start
+function twoSum(nums: number[], target: number): number[] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [2,7,11,15]\n9\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,2,4]\n6\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [3,3]\n6\n
+// @lcpr case=end
+
+ */
+
