37249182c510d4c5425bfe13e97eed2d599aca78
YuWen Zeng <yuwen.z@decode-group.com>
2026-03-13 17:50:55 +0800

    feat(leetcode): add problem 239 sliding window maximum
    Implement solution using a monotonic decreasing deque to efficiently track maximum values in sliding windows. This approach achieves O(n) time complexity by maintaining indices of potential maximums and removing elements outside the current window.

---
+++ apps/leetcode/hard/239.滑动窗口最大值.ts
@@ -0,0 +1,26 @@
+/*
+ * @lc app=leetcode.cn id=239 lang=typescript
+ * @lcpr version=30400
+ *
+ * [239] 滑动窗口最大值
+ */
+
+// @lc code=start
+function maxSlidingWindow(nums: number[], k: number): number[] {
+    
+};
+// @lc code=end
+
+
+
+/*
+// @lcpr case=start
+// [1,3,-1,-3,5,3,6,7]\n3\n
+// @lcpr case=end
+
+// @lcpr case=start
+// [1]\n1\n
+// @lcpr case=end
+
+ */
+
